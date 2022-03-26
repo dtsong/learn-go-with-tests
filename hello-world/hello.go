62dcb3c4887c357a494ffba3a5254c0e707d3eef
Daniel Song <xdtsong@gmail.com>
2022-03-26 13:54:03 -0700

    Refactor to a hello you

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world"))
 }
 
-func Hello() string {
-	return "Hello world!"
+func Hello(name string) string {
+	return "Hello " + name + "!"
 }
