758c4542908a6919c3549219d666e3e1bb117aba
Daniel Song <xdtsong@gmail.com>
2022-03-26 14:26:52 -0700

    move precommit

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -17,14 +17,17 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
