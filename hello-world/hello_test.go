493088d7f1db52c15e016c85192695eefd21b0ac
Daniel Song <xdtsong@gmail.com>
2022-03-26 14:06:11 -0700

    Refactor tests to have common assertCorrectMessage

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,21 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
 	t.Run("Say hello to someone", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello Chris!"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello World!' when an empty string is passed", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello World!"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
