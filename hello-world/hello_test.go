33da60aaf499050b1b09913dacb1511b1f301c49
Daniel Song <xdtsong@gmail.com>
2022-03-26 14:03:49 -0700

    Add breaking test for default Hello World use case

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello Chris!"
+	t.Run("Say hello to someone", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello Chris!"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Say 'Hello World!' when an empty string is passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World!"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
