6f995837ff56b84bd2d4eff446bdb6503faa25d8
Daniel Song <xdtsong@gmail.com>
2022-03-26 13:56:10 -0700

    Use a constant for the hello prefix

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -6,6 +6,8 @@ func main() {
 	fmt.Println(Hello("world"))
 }
 
+const englishHelloPrefix = "Hello "
+
 func Hello(name string) string {
-	return "Hello " + name + "!"
+	return englishHelloPrefix + name + "!"
 }
